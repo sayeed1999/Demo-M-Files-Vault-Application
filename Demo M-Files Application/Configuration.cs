99906db440705b009b2579aafad5a6c65e9737eb
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-04-16 19:08:22 +0600

    configuration files added for vault

--- Demo M-Files Application/Configuration.cs
+++ Demo M-Files Application/Configuration.cs
@@ -9,5 +9,23 @@ namespace Demo_M_Files_Application
     [DataContract]
     public class Configuration
     {
+        // const works like static variables!
+        private const string defaultValue = "My Default Value";
+        
+        [DataMember]
+        [TextEditor(DefaultValue = Configuration.defaultValue)]
+        public string TextValueWithDefault { get; set; } = Configuration.defaultValue;
+
+        [DataMember]
+        public string Username { get; set; }
+
+        [DataMember]
+        [Security(IsPassword = true)]
+        public string Password { get; set; }
+
+        // This member is not visible to vault administrators.
+        [DataMember]
+        [Security(ChangeBy = SecurityAttribute.UserLevel.SystemAdmin)]
+        public string WebAddress { get; set; }
     }
 }
