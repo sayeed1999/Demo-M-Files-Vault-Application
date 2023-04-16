4ddaa63de9cb6d91cc62184f56865ebcf657343c
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-04-16 19:05:59 +0600

    initialized repo

---
+++ Demo M-Files Application/Configuration.cs
@@ -0,0 +1,13 @@
+using MFiles.VAF.Configuration;
+using MFiles.VAF.Configuration.JsonAdaptor;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+
+namespace Demo_M_Files_Application
+{
+    [DataContract]
+    public class Configuration
+    {
+    }
+}
