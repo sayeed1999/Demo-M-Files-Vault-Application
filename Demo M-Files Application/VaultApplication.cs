d81ba9ed5eb361f7a29b6caa91fe1390fca62ea1
Md. Sayeed Rahman <mdsayeedrahman1999@gmail.com>
2023-04-17 17:51:45 +0600

    vault application overriden configurations taken into partial class for easier maintainability

--- Demo M-Files Application/VaultApplication.cs
+++ Demo M-Files Application/VaultApplication.cs
@@ -25,60 +25,9 @@ namespace Demo_M_Files_Application
     /// The entry point for this Vault Application Framework application.
     /// </summary>
     /// <remarks>Examples and further information available on the developer portal: http://developer.m-files.com/. </remarks>
-    public class VaultApplication
+    public partial class VaultApplication
         : ConfigurableVaultApplicationBase<Configuration>
     {
-        #region overrides
-        protected override void OnConfigurationUpdated(Configuration oldConfiguration, bool isValid, bool updateExternals)
-        {
-            base.OnConfigurationUpdated(oldConfiguration, isValid, updateExternals);
-
-            // Build up the string to log.
-            var stringBuilder = new StringBuilder();
-            stringBuilder.AppendLine("Configuration changed:");
-            stringBuilder.AppendLine($"Old: {JsonConvert.SerializeObject(oldConfiguration, Formatting.Indented)}");
-            stringBuilder.AppendLine($"New: {JsonConvert.SerializeObject(this.Configuration, Formatting.Indented)}");
-
-            // Log the string.
-            SysUtils.ReportToEventLog(
-                stringBuilder.ToString(),
-                EventLogEntryType.Information
-            );
-
-        }
-
-        protected override IEnumerable<ValidationFinding> CustomValidation(Vault vault, Configuration config)
-        {
-            // The base implementation should not return any, but handle it in case that changes in the future.
-            var validationFindings =
-                new List<ValidationFinding>(base.CustomValidation(vault, config) ?? new List<ValidationFinding>());
-
-            // Sanity.
-            if (null == config)
-                config = new Configuration();
-
-            // Username must be set.
-            if (string.IsNullOrWhiteSpace(config.Username))
-                validationFindings.Add(new ValidationFinding(
-                    ValidationFindingType.Error,
-                    nameof(this.Configuration.Username),
-                    "Username cannot be empty"));
-
-            // Password must be set.
-            if (string.IsNullOrWhiteSpace(config.Password))
-                validationFindings.Add(new ValidationFinding(
-                    ValidationFindingType.Error,
-                    nameof(this.Configuration.Password),
-                    "Password cannot be empty"));
-
-            return validationFindings;
-        }
-
-        public override string GetDashboardContent(IConfigurationRequestContext context)
-        {
-            return $"<h3>This is my dashboard.</h3>";
-        }
-        #endregion
         
         [EventHandler(MFEventHandlerType.MFEventHandlerBeforeCreateNewObjectFinalize)]
         public void MyEventHandler(EventHandlerEnvironment env)
---
+++ Demo M-Files Application/VaultApplicationOverrides.cs
@@ -0,0 +1,68 @@
+﻿using MFiles.VAF.Common;
+using MFiles.VAF.Configuration.AdminConfigurations;
+using MFiles.VAF.Configuration;
+using MFilesAPI;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_M_Files_Application
+{
+    public partial class VaultApplication
+    {
+        protected override void OnConfigurationUpdated(Configuration oldConfiguration, bool isValid, bool updateExternals)
+        {
+            base.OnConfigurationUpdated(oldConfiguration, isValid, updateExternals);
+
+            // Build up the string to log.
+            var stringBuilder = new StringBuilder();
+            stringBuilder.AppendLine("Configuration changed:");
+            stringBuilder.AppendLine($"Old: {JsonConvert.SerializeObject(oldConfiguration, Formatting.Indented)}");
+            stringBuilder.AppendLine($"New: {JsonConvert.SerializeObject(this.Configuration, Formatting.Indented)}");
+
+            // Log the string.
+            SysUtils.ReportToEventLog(
+                stringBuilder.ToString(),
+                EventLogEntryType.Information
+            );
+
+        }
+
+        protected override IEnumerable<ValidationFinding> CustomValidation(Vault vault, Configuration config)
+        {
+            // The base implementation should not return any, but handle it in case that changes in the future.
+            var validationFindings =
+                new List<ValidationFinding>(base.CustomValidation(vault, config) ?? new List<ValidationFinding>());
+
+            // Sanity.
+            if (null == config)
+                config = new Configuration();
+
+            // Username must be set.
+            if (string.IsNullOrWhiteSpace(config.Username))
+                validationFindings.Add(new ValidationFinding(
+                    ValidationFindingType.Error,
+                    nameof(this.Configuration.Username),
+                    "Username cannot be empty"));
+
+            // Password must be set.
+            if (string.IsNullOrWhiteSpace(config.Password))
+                validationFindings.Add(new ValidationFinding(
+                    ValidationFindingType.Error,
+                    nameof(this.Configuration.Password),
+                    "Password cannot be empty"));
+
+            return validationFindings;
+        }
+
+        public override string GetDashboardContent(IConfigurationRequestContext context)
+        {
+            return $"<h3>This is my dashboard.</h3>";
+        }
+
+    }
+}
